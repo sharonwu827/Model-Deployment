91acca528e8e77607c8712681ce518ab2dcb2f2b
Sharon Wu <sharonwu827@gmail.com>
2021-10-26 14:10:12 -0700

    Update

--- deploying-machine-learning-models-master/packages/regression_model/regression_model/processing/data_management.py
+++ deploying-machine-learning-models-master/packages/regression_model/regression_model/processing/data_management.py
@@ -1,24 +1,23 @@
-import typing as t
-from pathlib import Path
-
-import joblib
 import pandas as pd
+import joblib
 from sklearn.pipeline import Pipeline
 
+from regression_model.config import config
 from regression_model import __version__ as _version
-from regression_model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
+
+import logging
+import typing as t
+
+
+_logger = logging.getLogger(__name__)
 
 
 def load_dataset(*, file_name: str) -> pd.DataFrame:
-    dataframe = pd.read_csv(Path(f"{DATASET_DIR}/{file_name}"))
-    dataframe["MSSubClass"] = dataframe["MSSubClass"].astype("O")
-
-    # rename variables beginning with numbers to avoid syntax errors later
-    transformed = dataframe.rename(columns=config.model_config.variables_to_rename)
-    return transformed
+    _data = pd.read_csv(f"{config.DATASET_DIR}/{file_name}")
+    return _data
 
 
-def save_pipeline(*, pipeline_to_persist: Pipeline) -> None:
+def save_pipeline(*, pipeline_to_persist) -> None:
     """Persist the pipeline.
     Saves the versioned model, and overwrites any previous
     saved models. This ensures that when the package is
@@ -27,17 +26,18 @@ def save_pipeline(*, pipeline_to_persist: Pipeline) -> None:
     """
 
     # Prepare versioned save file name
-    save_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
-    save_path = TRAINED_MODEL_DIR / save_file_name
+    save_file_name = f"{config.PIPELINE_SAVE_FILE}{_version}.pkl"
+    save_path = config.TRAINED_MODEL_DIR / save_file_name
 
     remove_old_pipelines(files_to_keep=[save_file_name])
     joblib.dump(pipeline_to_persist, save_path)
+    _logger.info(f"saved pipeline: {save_file_name}")
 
 
 def load_pipeline(*, file_name: str) -> Pipeline:
     """Load a persisted pipeline."""
 
-    file_path = TRAINED_MODEL_DIR / file_name
+    file_path = config.TRAINED_MODEL_DIR / file_name
     trained_model = joblib.load(filename=file_path)
     return trained_model
 
@@ -45,11 +45,14 @@ def load_pipeline(*, file_name: str) -> Pipeline:
 def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
     """
     Remove old model pipelines.
+
     This is to ensure there is a simple one-to-one
     mapping between the package version and the model
     version to be imported and used by other applications.
+    However, we do also include the immediate previous
+    pipeline version for differential testing purposes.
     """
-    do_not_delete = files_to_keep + ["__init__.py"]
-    for model_file in TRAINED_MODEL_DIR.iterdir():
+    do_not_delete = files_to_keep + ['__init__.py']
+    for model_file in config.TRAINED_MODEL_DIR.iterdir():
         if model_file.name not in do_not_delete:
             model_file.unlink()
